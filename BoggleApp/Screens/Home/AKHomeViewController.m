d4cab2033deb13a78b38b71e24586300f18b5a38
Atul Khatri <atulkhatri@live.com>
2017-11-22 14:39:37 +0530

    Added table view

--- BoggleApp/Screens/Home/AKHomeViewController.m
+++ BoggleApp/Screens/Home/AKHomeViewController.m
@@ -14,8 +14,12 @@
 #define kSampleDictionary @[@"bred", @"yore", @"byre", @"abed", @"oread", @"bore", @"orby", @"robed", @"broad", @"byroad", @"robe", @"bored", @"derby", @"bade", @"aero", @"read", @"orbed", @"verb", @"aery", @"bead", @"bread", @"very", @"road", @"robbed", @"robber", @"board", @"dove", @"rob", @"ore", @"abe", @"bad"]
 #define kMinimumWordLength 4
 
-@interface AKHomeViewController ()
+#define kCellReuseIdentifier @"HomeCell"
 
+@interface AKHomeViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UILabel *countLabel;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray* words;
 @end
 
 @implementation AKHomeViewController
@@ -24,8 +28,30 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    NSArray* words = [[AKBoggleManager sharedInstance] findWordsInBoard:kSampleBoard ofDimension:kSampleDimension withDictionary:kSampleDictionary andMinimumWordLength:kMinimumWordLength];
-    NSLog(@"Words: %@", words);
+    self.words = [[AKBoggleManager sharedInstance] findWordsInBoard:kSampleBoard ofDimension:kSampleDimension withDictionary:kSampleDictionary andMinimumWordLength:kMinimumWordLength];
+    self.countLabel.text = [NSString stringWithFormat:@"%ld %@ FOUND!",self.words.count, (self.words.count==1)?@"WORD": @"WORDS"];
+    if(self.words.count){
+        [self.tableView reloadData];
+    }
+}
+
+#pragma mark - UITableViewDataSource methods
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell =  [tableView dequeueReusableCellWithIdentifier:kCellReuseIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellReuseIdentifier];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld.\t%@",indexPath.row+1,[self.words objectAtIndex:indexPath.row]];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.words.count;
+}
+
+#pragma mark - UITableViewDelegate methods
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
